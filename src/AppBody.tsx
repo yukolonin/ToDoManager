fff32372af495835bc1aa1c7c3d6c156deebf682
yukolonin <yukolonin@gmail.com>
2020-08-14 23:23:10 +0700

    First phase of mobile view implementation; added resize event

--- src/AppBody.tsx
+++ src/AppBody.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useLayoutEffect, useState} from 'react';
 import './App.css';
 
 import Sidebar from './Sidebar'
@@ -14,19 +14,49 @@ import TaskList from "./TaskList";
 *
 * */
 
+
+function useWindowSize() {
+    const [size, setSize] = useState([0, 0]);
+    useLayoutEffect(() => {
+        function updateSize() {
+            setSize([window.innerWidth, window.innerHeight]);
+        }
+        window.addEventListener('resize', updateSize);
+        updateSize();
+        return () => window.removeEventListener('resize', updateSize);
+    }, []);
+    return size;
+}
+
 function AppBody() {
     const{state, dispatch} = React.useContext(TaskListContext);
+
+    const [width, height] = useWindowSize();
+
+    // TODO: Change values to relevant
+    const widthThreshold = 666;
+    const heightThreshold = 777;
 
     useEffect(() => {
         localStorage.setItem('taskList', JSON.stringify(state.taskList))
     })
 
+
+    // TODO: Roadmap:
+    // TODO: Separate components for reuse: Sidebar-list, TaskList, SideMenu-dialogue
+    // TODO: Create Mobile Layout skeleton (AppBar+Toolbar, Drawer, %pop-up dialog%)
+    // TODO: Fill it with separated components
+
     return (
-        <div className='main-frame'>
-            <Sidebar />
-            <TaskList />
-            {state.isMenuOn && <SideMenu />}
-        </div>
+        width > widthThreshold && height > heightThreshold
+            ?
+            <div className='main-frame'>
+                <Sidebar />
+                <TaskList />
+                {state.isMenuOn && <SideMenu />}
+            </div>
+            :
+            <div>HERE BE MOBILE VIEW</div>
     )
 }
 
